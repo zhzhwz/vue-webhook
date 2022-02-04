006533fd290fe634a87e3df5e8cace3e27287457
zhzhwz <954753124@qq.com>
2022-02-04 15:10:04 +0800

    add vue-front.sh

---
+++ vue-front.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORK_PATH='/usr/projects/vue-front'
+cd $WORK_PATH
+echo "clearing old codes..."
+git reset --hard origin/master
+git clean -f
+echo "pulling newest codes..."
+git pull origin master
+echo "compiling..."
+npm run build
+echo "building..."
+docker build -t vue-front .
+echo "stoping and deleting old container..."
+docker stop vue-front-container
+docker rm vue-front-container
+echo "starting new container..."
+docker container run -p 8080:80 --name vue-back-container -d vue-back
