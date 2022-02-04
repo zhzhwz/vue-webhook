459f25b4374456f617615d9bb88499b2fbe65ff5
zhzhwz <954753124@qq.com>
2022-02-04 14:52:58 +0800

    add vue-back.sh

---
+++ vue-back.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_PATH = '/usr/projects/vue-back'
+cd $WORK_PATH
+echo "clear old codes..."
+git reset --hard origin/master
+git clean -f
+echo "pull newest codes..."
+git pull origin master
+echo "building..."
+docker build -t vue-back .
+echo "stop and delete old container..."
+docker stop vue-back-container
+docker rm vue-back-container
+echo "start new container..."
+docker container run -p 3000:3000 --name vue-back-container -d vue-back
