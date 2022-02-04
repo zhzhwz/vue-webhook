a5d0c8abe14a300f97e255eaacde2439feea1038
zhzhwz <954753124@qq.com>
2022-02-04 15:31:09 +0800

    add auto rebuild

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const crypto = require('crypto');
+const spawn = require('child_process')
 const SECRET = '123456';
 function sign(body) {
     return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex');
@@ -18,9 +19,21 @@ const server = http.createServer(function(request, response) {
             if (signature !== sign(body)) {
                 return response.end('Not Allowed');
             }
+            response.setHeader('Content-Type', 'application-json');
+            response.end(JSON.stringify({ ok: true }));
+            if (event === 'push') {
+                let payload = JSON.parse(body);
+                let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+                let buffers = [];
+                child.stdout.on('data', function(buffer) {
+                    buffers.push(buffer);
+                });
+                child.stdout.on('end', function(buffer) {
+                    let log = Buffer.concat(buffers);
+                    console.log(log);
+                });
+            }
         })
-        response.setHeader('Content-Type', 'application-json');
-        response.end(JSON.stringify({ok: true}));
     }
     else {
         response.end('Not Found');
